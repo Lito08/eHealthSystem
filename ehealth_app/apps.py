55d02a915c2c1e1f016a72a24ba496fc8a96f9a6
L I T O <76776795+Lito08@users.noreply.github.com>
2024-12-11 19:02:02 +0800

    Fresh Install

--- ehealth_app/apps.py
+++
@@ -1,6 +0,0 @@
-
-from django.apps import AppConfig
-
-class Ehealth_appConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ehealth_app'
--- ehealth_app/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import UploadedFile
-
-class FileUploadForm(forms.ModelForm):
-    class Meta:
-        model = UploadedFile
-        fields = ['file']  # or any other fields you want to include
