2b80488fa97bcee04b1ad966b7db74b0afcc5fa9
L I T O <76776795+Lito08@users.noreply.github.com>
2025-01-26 13:40:13 +0800

    Deleted sql for creating fresh for the whole system

--- ehealth_system/ehealth_system/settings.py
+++ ehealth_system/ehealth_system/settings.py
@@ -27,17 +27,19 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'users.CustomUser'
 
 # Application definition
 
 INSTALLED_APPS = [
+    'users',
+    'appointments',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'appointments',
 ]
 
 MIDDLEWARE = [
